d8562eb7a4460d2854de1af00e1c25a98bef9a09
markus <markus@a12a6f42-3511-41a4-a558-ac00e9fe004e>
2017-01-10 19:33:26 +0000

    changed dir structure

--- trunk/u2/avanode/file/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'markus'
-
-from .fileentry import FileEntry
-from .fileconfig import FileConfig
-from .graphviz import GraphViz
-
--- trunk/u2/avanode/protocol/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'markus'
-
-from .abstractcommand import AbstractCommand
-from .commandfactory import CommandFactory
-from .init import INIT
-from .message import MESSAGE
-from .shutdown import SHUTDOWN
